f23c62c84afc954d24651cbd00f83d9ea7c41dd5
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-08 14:11:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,12 @@
 #write your code here
 
-def countdown
-  
+def countdown(int)
+  x = int
+while x > 0
+  puts "#{number} SECOND(S)!"
+  x -= 1
+end
+"HAPPY NEW YEAR!"
 end
 
 x = 1
@@ -9,6 +14,8 @@ while x < 10
   puts "#{x} is less than 10"
   x += 1
 end
+
+3. Write a method that takes in an integer argument and uses a while loop to countdown from that integer to `0`, outputting `"#{number} SECOND(S)!"` in each iteration of the loop. The method should return `"HAPPY NEW YEAR!"` after the loop finishes. **Hint:** *In Ruby, a method will return the very last line of code that it executes.*
 
 def countdown_with_sleep
   
